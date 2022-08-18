b4797633aacd8b315e8a2b5c922784f011583afe
Benjamin BRASSART <bbrassar@student.42.fr>
2022-08-18 06:05:29 +0200

    Add SFINAE stuff

--- src/utils.hpp
+++ src/utils.hpp
@@ -6,14 +6,79 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 02:49:15 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/08/18 03:18:27 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/08/18 05:43:43 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <stdint.h>
+
 namespace ft
 {
+	template< bool Cond, class T = void >
+	struct enable_if {};
+
+	template< class T >
+	struct enable_if< true, T >
+	{
+		typedef T type;
+	};
+
+	template< class T, T v >
+	struct integral_constant
+	{
+		static T const value = v;
+		typedef T value_type;
+		typedef integral_constant< T, v > type;
+		operator T() { return v; }
+	};
+
+	typedef integral_constant< bool, true > true_type;
+	typedef integral_constant< bool, false > false_type;
+
+	template< class T >
+	struct is_integral : public false_type {};
+
+	template< >
+	struct is_integral< bool > : public true_type {};
+
+	template< >
+	struct is_integral< char > : public true_type {};
+
+	template< >
+	struct is_integral< wchar_t > : public true_type {};
+
+	template< >
+	struct is_integral< signed char > : public true_type {};
+
+	template< >
+	struct is_integral< short int > : public true_type {};
+
+	template< >
+	struct is_integral< int > : public true_type {};
+
+	template< >
+	struct is_integral< long int > : public true_type {};
+
+	template< >
+	struct is_integral< long long int > : public true_type {};
+
+	template< >
+	struct is_integral< unsigned char > : public true_type {};
+
+	template< >
+	struct is_integral< unsigned short int > : public true_type {};
+
+	template< >
+	struct is_integral< unsigned int > : public true_type {};
+
+	template< >
+	struct is_integral< unsigned long int > : public true_type {};
+
+	template< >
+	struct is_integral< unsigned long long int > : public true_type {};
+
 	template< class T1, class T2 >
 	struct pair
 	{
