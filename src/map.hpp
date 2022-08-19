7344dfa400bda4d93ae4e19c4118cf15247f214d
Benjamin BRASSART <bbrassar@student.42.fr>
2022-08-19 22:56:39 +0200

    Add precision on TODO comments

--- src/map.hpp
+++ src/map.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 02:45:49 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/08/19 22:55:48 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/08/19 22:56:30 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,14 @@ namespace ft
 		typedef T mapped_type;
 		typedef ft::pair< key_type const, mapped_type > value_type;
 		typedef Compare key_compare;
-		typedef void* value_compare; // TODO
+		typedef void* value_compare; // TODO implement value comparator
 		typedef Alloc allocator_type;
 		typedef typename allocator_type::reference reference;
 		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::const_pointer const_pointer;
-		typedef value_type* iterator; // TODO
-		typedef value_type const* const_iterator; // TODO
+		typedef value_type* iterator; // TODO implement bidirectional iterator
+		typedef value_type const* const_iterator; // TODO implement bidirectional iterator
 		typedef ft::reverse_iterator< iterator > reverse_iterator;
 		typedef ft::reverse_iterator< const_iterator > const_reverse_iterator;
 		typedef typename ft::iterator_traits< iterator >::difference_type difference_type;
