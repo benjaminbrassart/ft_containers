2eaf4c6462e09c8f3e2693b54a1dea095872c258
Benjamin BRASSART <bbrassar@student.42.fr>
2022-08-20 03:53:53 +0200

    Add implementaion for vector::vector() Add implementation for vector::reserve(size_type) Rename vector private members

--- src/vector.hpp
+++ src/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 02:00:15 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/08/20 03:40:07 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/08/20 03:54:32 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,19 @@ namespace ft
 		typedef typename allocator_type::size_type size_type;
 
 		private:
-		allocator_type alloc;
-		pointer data_ptr;
-		size_type count_current;
-		size_type count_max;
+		allocator_type _alloc;
+		pointer _data;
+		size_type _size;
+		size_type _capacity;
 
 		public:
-		// TODO provide implementation
-		explicit vector(allocator_type const& alloc = allocator_type());
+		// TODO test implementation
+		explicit vector(allocator_type const& alloc = allocator_type()) :
+			_alloc(alloc),
+			_data(),
+			_size(0),
+			_capacity(0)
+		{}
 
 		// TODO provide implementation
 		explicit vector(size_type n, value_type const& val = value_type(), allocator_type const& alloc = allocator_type());
@@ -65,32 +70,32 @@ namespace ft
 		~vector()
 		{
 			this->clear();
-			this->alloc.deallocate(this->data_ptr, this->count_max);
+			this->_alloc.deallocate(this->_data, this->capacity());
 		}
 
 		public:
 		// TODO test implementation
 		iterator begin()
 		{
-			return this->data_ptr;
+			return this->_data;
 		}
 
 		// TODO test implementation
 		const_iterator begin() const
 		{
-			return this->data_ptr;
+			return this->_data;
 		}
 
 		// TODO test implementation
 		iterator end()
 		{
-			return this->alloc.address(this->data_ptr[this->count_current]);
+			return this->_alloc.address(this->operator[](this->size()));
 		}
 
 		// TODO test implementation
 		const_iterator end() const
 		{
-			return this->alloc.address(this->data_ptr[this->count_current]);
+			return this->_alloc.address(this->_data[this->_size]);
 		}
 
 		// TODO provide implementation
@@ -106,12 +111,12 @@ namespace ft
 		public:
 		size_type size() const
 		{
-			return this->count_current;
+			return this->_size;
 		}
 
 		size_type max_size() const
 		{
-			return this->alloc.max_size();
+			return this->_alloc.max_size();
 		}
 
 		// TODO provide implementation
@@ -119,44 +124,58 @@ namespace ft
 
 		size_type capacity() const
 		{
-			return this->count_max;
+			return this->_capacity;
 		}
 
 		bool empty() const
 		{
-			return this->count_current == 0;
+			return this->_size == 0;
 		}
 
-		// TODO provide implementation
-		void reserve(size_type n);
+		// TODO test implementation
+		void reserve(size_type n)
+		{
+			if (this->capacity() >= n)
+				return ;
+
+			pointer new__data = this->_alloc.allocate(n * 2);
+
+			for (size_type i; i < this->size(); ++i)
+			{
+				this->_alloc.construct(this->_alloc.address(new__data[i]), this->_data[i]);
+				this->_alloc.destroy(this->_alloc.address(this->_data[i]));
+			}
+			this->_alloc.deallocate(this->_data, this->capacity());
+			this->_capacity = n;
+		}
 
 		public:
 		// TODO test implementation
 		reference operator[](size_type n)
 		{
-			return this->data_ptr[n];
+			return this->_data[n];
 		}
 
 		// TODO test implementation
 		const_reference operator[](size_type n) const
 		{
-			return this->data_ptr[n];
+			return this->_data[n];
 		}
 
 		// TODO test implementation
 		reference at(size_type n)
 		{
-			if (n >= this->count_current)
+			if (n >= this->_size)
 				throw 0; // TODO throw real exception
-			return this->data_ptr[n];
+			return this->_data[n];
 		}
 
 		// TODO test implementation
 		const_reference at(size_type n) const
 		{
-			if (n >= this->count_current)
+			if (n >= this->_size)
 				throw 0; // TODO throw real exception
-			return this->data_ptr[n];
+			return this->_data[n];
 		}
 
 		// TODO provide implementation
@@ -205,15 +224,15 @@ namespace ft
 		// TODO test implementation
 		void clear()
 		{
-			for (size_type n = 0; n < this->count_current; ++n)
-				this->alloc.destroy(&this->alloc.address(data_ptr[n]));
-			this->count_current = 0;
+			for (size_type n = 0; n < this->_size; ++n)
+				this->_alloc.destroy(this->_alloc.address(_data[n]));
+			this->_size = 0;
 		}
 
 		public:
 		allocator_type get_allocator() const
 		{
-			return this->alloc;
+			return this->_alloc;
 		}
 	}; // class vector
 
