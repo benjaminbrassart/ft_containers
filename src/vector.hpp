f5bc2171b728597cb851b061b098cf433d1cf6f9
Benjamin BRASSART <bbrassar@student.42.fr>
2022-08-20 03:40:26 +0200

    Add some vector member function implementation

--- src/vector.hpp
+++ src/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 02:00:15 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/08/20 03:20:45 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/08/20 03:40:07 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ namespace ft
 	{
 		public:
 		typedef T value_type;
-		typedef Alloc allocator_type;
+		typedef std::allocator< T > allocator_type; // TODO revert to Alloc
 		typedef typename allocator_type::reference reference;
 		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer pointer;
@@ -61,19 +61,37 @@ namespace ft
 		// TODO provide implementation
 		vector& operator=(vector const& x);
 
-		// TODO provide implementation
-		~vector();
+		// TODO test implementation
+		~vector()
+		{
+			this->clear();
+			this->alloc.deallocate(this->data_ptr, this->count_max);
+		}
 
 		public:
-		// TODO provide implementation
-		iterator begin();
-		// TODO provide implementation
-		const_iterator begin() const;
+		// TODO test implementation
+		iterator begin()
+		{
+			return this->data_ptr;
+		}
 
-		// TODO provide implementation
-		iterator end();
-		// TODO provide implementation
-		const_iterator end() const;
+		// TODO test implementation
+		const_iterator begin() const
+		{
+			return this->data_ptr;
+		}
+
+		// TODO test implementation
+		iterator end()
+		{
+			return this->alloc.address(this->data_ptr[this->count_current]);
+		}
+
+		// TODO test implementation
+		const_iterator end() const
+		{
+			return this->alloc.address(this->data_ptr[this->count_current]);
+		}
 
 		// TODO provide implementation
 		reverse_iterator rbegin();
@@ -86,34 +104,60 @@ namespace ft
 		const_reverse_iterator rend() const;
 
 		public:
-		// TODO provide implementation
-		size_type size() const;
+		size_type size() const
+		{
+			return this->count_current;
+		}
 
-		// TODO provide implementation
-		size_type max_size() const;
+		size_type max_size() const
+		{
+			return this->alloc.max_size();
+		}
 
 		// TODO provide implementation
 		void resize(size_type n, value_type val = value_type());
 
-		// TODO provide implementation
-		size_type capacity() const;
+		size_type capacity() const
+		{
+			return this->count_max;
+		}
 
-		// TODO provide implementation
-		bool empty() const;
+		bool empty() const
+		{
+			return this->count_current == 0;
+		}
 
 		// TODO provide implementation
 		void reserve(size_type n);
 
 		public:
-		// TODO provide implementation
-		reference operator[](size_type n);
-		// TODO provide implementation
-		const_reference operator[](size_type n) const;
+		// TODO test implementation
+		reference operator[](size_type n)
+		{
+			return this->data_ptr[n];
+		}
 
-		// TODO provide implementation
-		reference at(size_type n);
-		// TODO provide implementation
-		const_reference at(size_type n) const;
+		// TODO test implementation
+		const_reference operator[](size_type n) const
+		{
+			return this->data_ptr[n];
+		}
+
+		// TODO test implementation
+		reference at(size_type n)
+		{
+			if (n >= this->count_current)
+				throw 0; // TODO throw real exception
+			return this->data_ptr[n];
+		}
+
+		// TODO test implementation
+		const_reference at(size_type n) const
+		{
+			if (n >= this->count_current)
+				throw 0; // TODO throw real exception
+			return this->data_ptr[n];
+		}
 
 		// TODO provide implementation
 		reference front();
@@ -158,12 +202,19 @@ namespace ft
 		// TODO provide implementation
 		void swap(vector& x);
 
-		// TODO provide implementation
-		void clear();
+		// TODO test implementation
+		void clear()
+		{
+			for (size_type n = 0; n < this->count_current; ++n)
+				this->alloc.destroy(&this->alloc.address(data_ptr[n]));
+			this->count_current = 0;
+		}
 
 		public:
-		// TODO provide implementation
-		allocator_type get_allocator() const;
+		allocator_type get_allocator() const
+		{
+			return this->alloc;
+		}
 	}; // class vector
 
 	// TODO provide implementation
