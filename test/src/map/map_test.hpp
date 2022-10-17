1fafa92558db421d84ec0cd0e9a95a457ebf3f9d
Benjamin BRASSART <bbrassar@student.42.fr>
2022-10-17 09:08:43 +0200

    Add map_test header

---
+++ test/src/map/map_test.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../tester.hpp"
+
+#include HEADER_MAP
+
+#define map NAMESPACE::map
+
+template< class Key, class Value, class Compare, class Alloc >
+void print_vector(map< Key, Value, Compare, Alloc > const& m)
+{
+	typename map< Key, Value, Compare, Alloc >::const_iterator it;
+	typename map< Key, Value, Compare, Alloc >::const_reverse_iterator rit;
+
+	std::cout
+		<< "size: " << m.size() << '\n'
+		<< "content:" << '\n';
+
+	for (it = m.begin(); it != m.end(); ++it)
+		std::cout << "  [" << it->first << "] = " << it->second << '\n';
+
+	std::cout
+		<< '\n'
+		<< "content: (reverse):" << '\n';
+
+	for (rit = m.rbegin(); rit != m.rend(); ++rit)
+		std::cout << "  [" << rit->first << "] = " << it->second << '\n';
+	std::cout << '\n';
+}
