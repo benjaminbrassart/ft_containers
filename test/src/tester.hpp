35b116b947c318cbf60a249d17d86311c0a862d5
Benjamin BRASSART <bbrassar@student.42.fr>
2022-10-17 09:27:50 +0200

    Fix namespaces

--- test/src/tester.hpp
+++ test/src/tester.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 23:48:53 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/10/17 09:14:38 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/10/17 09:24:48 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,16 @@
 
 #endif
 
-using namespace NAMESPACE;
-
 # ifndef SEED
 #  define SEED 21U
 # endif
+
+namespace NAMESPACE {};
+using namespace NAMESPACE;
 
 #define DESCRIBE(Body) \
 int main() { \
 srand(SEED); \
 std::cout << "Test: " << __FILE__ << '\n' << '\n'; \
 Body; \
-} \
+}
