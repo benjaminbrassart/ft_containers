6655c09f2f241cb0a7121243b1621cde6c499324
Benjamin BRASSART <bbrassar@student.42.fr>
2022-11-03 13:42:10 +0100

    change some stuff in enable_if test

--- test/src/enable_if/enable_if.cpp
+++ test/src/enable_if/enable_if.cpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 17:20:26 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/10/19 12:02:12 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/03 13:41:18 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,13 @@ struct enable_if_integer : public ft::enable_if< std::numeric_limits< T >::is_in
 
 int main()
 {
-	enable_if_integral< char >::type c(L'c');
-	enable_if_integer< char >::type d(L'd');
+	enable_if_integral< wchar_t >::type c(L'c');
+	enable_if_integer< wchar_t >::type d(L'd');
+	enable_if_integral< wchar_t const >::type cc(L'c');
+	enable_if_integer< wchar_t const >::type dc(L'd');
 
-	std::cout << "c: " << c << '\n' << "d: " << d << '\n';
+	// enable_if_integral< char* >::type s(""); // <-- wont compile because pointer are not numeric types
+
+	std::wcout << "c: " << c << '\n' << "d: " << d << '\n';
+	std::wcout << "cc: " << cc << '\n' << "dc: " << dc << '\n';
 }
